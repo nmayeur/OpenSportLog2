3f0eca6f4b1c4da64d8cbff5430e6fb66857317a
Nicolas Mayeur <mail@mayeur.com>
2023-09-30 10:23:47 +0200

    Added database project

---
+++ Infrastructure/OslSqlServer/dbo/Tables/Tracks.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Tracks] (
+    [Id]         INT  IDENTITY (1, 1) NOT NULL,
+    [Name]       TEXT NULL,
+    [ActivityId] INT  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_TrackEntity_ActivityEntity_ActivityEntityId] FOREIGN KEY ([ActivityId]) REFERENCES [dbo].[Activities] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_TrackEntity_ActivityEntityId]
+    ON [dbo].[Tracks]([ActivityId] ASC);
+
