f26623d8533046c4c0a4568c2e22db9cc7af3dfc
Nicolas Mayeur <mail@mayeur.com>
2023-10-09 21:23:27 +0200

    wip, reading activity details

--- Web/webspa/src/features/activities/components/ActivityDetails.tsx
+++ Web/webspa/src/features/activities/components/ActivityDetails.tsx
@@ -1,16 +1,25 @@
 import { Grid, Paper, TextField } from "@mui/material"
+import useFetchActivityById from "../hooks/FetchActivityById";
+import React from "react";
 
 interface ActivityDetailsProp {
     activityId: number | null;
 }
 
 export const ActivityDetails = (props: ActivityDetailsProp) => {
+
+    const [baseUrl] = React.useState(`https://osl-webapiapi-dev.azure-api.net/osl-dev/api`);
+    const [api_key] = React.useState("2d5915334aa74fb19fefe972c952c5d6");
+
+    const { activity } = useFetchActivityById(baseUrl, api_key, props.activityId);
+
     return (
         <Paper sx={{ width: '100%', overflow: 'hidden' }} elevation={3}>
             Activity {props.activityId}
             <Grid container spacing={3}>
                 <Grid item xs={6}>
                     <TextField id="athleteName" label="Nom" variant="standard" fullWidth
+                        value={activity.name}
                         InputProps={{
                             readOnly: true,
                         }} />
---
+++ Web/webspa/src/features/activities/hooks/FetchActivityById.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { IPaginatedItemsViewModel } from "../../../types/IPaginatedItemsViewModel";
+import { GridRowsProp } from "@mui/x-data-grid";
+
+
+interface IAthleteDto {
+    id: number;
+    name: string;
+}
+interface IActivityDto {
+    id: number;
+    name: string;
+    athlete: IAthleteDto;
+    location: string;
+}
+
+const useFetchActivityById = (baseUrl: string, api_key: string, activityId: number | null) => {
+    console.log(`Calling useFetchActivityById for athleteId ${activityId}`)
+
+    const [activity, setActivity] = useState<IActivityDto>({} as IActivityDto);
+    const [loading, setloading] = useState(true);
+    const [error, seterror] = useState("");
+
+    useEffect(() => {
+        if (activityId === null) {
+            console.error("activityId is null")
+            seterror("activityId is null")
+            return
+        }
+
+        const fetchData = async () => {
+            const url = `${baseUrl}/Activity/activityById?activityId=${activityId}`
+            console.log(`URL : ${url}`)
+            const requestHeaders: HeadersInit = new Headers();
+            requestHeaders.set('Ocp-Apim-Subscription-Key', api_key);
+            const data = await fetch(url, { headers: requestHeaders })
+            const dto = await data.json() as IActivityDto
+            setActivity(dto)
+            seterror("")
+            setloading(false)
+        }
+        fetchData().catch(error => {
+            if (error instanceof TypeError) {
+                console.error(error.message)
+                seterror(error.message)
+            } else if (error instanceof SyntaxError) {
+                console.error(error.message)
+                seterror(error.message)
+            }
+
+        });
+    }, [baseUrl, api_key, activityId]);
+
+    return { activity, loading, error };
+};
+
+export default useFetchActivityById;
