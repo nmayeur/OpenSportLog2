676f4ce7112b57cf45a61fc09d0a4b1c6928a01e
Nicolas Mayeur <mail@mayeur.com>
2023-10-18 19:26:20 +0200

    Implementing functional tests

---
+++ Web/WebAPI.Common/Infrastructure/Log/NLoggerService.cs
@@ -0,0 +1,42 @@
+﻿namespace WebAPI.Common.Infrastructure.Log
+{
+    public class NLoggerService : ILoggerService
+    {
+        private readonly NLog.ILogger _Logger;
+
+        public NLoggerService(string name)
+        {
+            _Logger = NLog.LogManager.GetLogger(name);
+        }
+
+        public bool IsDebugEnabled()
+        {
+            return _Logger.IsDebugEnabled;
+        }
+
+        void ILoggerService.Debug(string message)
+        {
+            _Logger.Debug(message);
+        }
+
+        void ILoggerService.Error(string message)
+        {
+            _Logger.Error(message);
+        }
+
+        void ILoggerService.Error(string message, Exception exception)
+        {
+            _Logger.Error(exception, message);
+        }
+
+        void ILoggerService.Info(string message)
+        {
+            _Logger.Info(message);
+        }
+
+        void ILoggerService.Warn(string message)
+        {
+            _Logger.Warn(message);
+        }
+    }
+}
