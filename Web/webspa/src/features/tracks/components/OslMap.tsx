186f4fe60f88cd11fcc7727c8e518bfa99d2e330
Nicolas Mayeur <mail@mayeur.com>
2023-10-27 17:54:35 +0200

    Introducing geoson

--- Web/webspa/src/features/tracks/components/OslMap.tsx
+++ Web/webspa/src/features/tracks/components/OslMap.tsx
@@ -1,6 +1,8 @@
 import { MapContainer, TileLayer } from "react-leaflet"
+import { OslMapTrack } from "./OslMapTrack"
 
 export const OslMap = () => {
+
 
     return (
         <MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false} id="oslmap">
@@ -8,5 +10,6 @@ export const OslMap = () => {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
-        </MapContainer>)
+            <OslMapTrack />
+        </MapContainer >)
 }
